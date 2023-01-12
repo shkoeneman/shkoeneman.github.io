99014612aee6a57ae47ae235913f5321533fd844
skoeneman <scott-koeneman@uiowa.edu>
2023-01-12 11:53:17 -0500

    Better script organization

---
+++ scripts/generate_previous_groups.R
@@ -0,0 +1,6 @@
+#Create list of lists of all groups
+all_dates <- list(
+  list(c("Scott","Alex","Jesse","Amy","Kelsey","Holly","Tara","Matt"), #1/11/2023
+       c("Arianna","Haley","Mike","Carol","Amanda"),
+       c("Martin","Tracy","Lauren","Sam"))
+)
