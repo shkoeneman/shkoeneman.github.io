0b445e851c77db8b975ab4fb453835e9eafafd9e
skoeneman <scott-koeneman@uiowa.edu>
2023-02-01 12:00:13 -0500

    Automatic update for 2023-02-01

--- docs/scripts/generate_previous_groups.R
+++ docs/scripts/generate_previous_groups.R
@@ -2,5 +2,8 @@
 all_dates <- list(
   list(c("Scott","Alex","Jesse","Amy","Kelsey","Holly","Tara","Matt"), #1/11/2023
        c("Arianna","Haley","Mike","Carol","Amanda"),
-       c("Martin","Tracy","Lauren","Sam"))
+       c("Martin","Tracy","Lauren","Sam")),
+  list(c("Alex","Jesse","Holly","Steve","Tara","Kelsey"), #1/25/2023
+       c("Haley","Arianna","Lauren","Tracy","Amy"),
+       c("Scott","Russ","Jordyn","Amanda"))
 )
